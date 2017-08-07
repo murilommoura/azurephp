13c45af3ff8a2f713ae6a736e411ff3ed6635385
murilommoura <murilommoura@users.noreply.github.com>
2017-08-07 03:12:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
-date_default_timezone_set('America/Los_Angeles');
+error_reporting(E_ALL);
+
+ini_set('display_errors', 1);
+
+ini_set("date.timezone", "America/Sao_Paulo");
+
+date_default_timezone_set('America/Sao_Paulo');
 
 echo 'Novo Teste';
 
